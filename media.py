0f08eb22f65be54fe3199aba84d6f8907e2e8186
Tamer <tamerthamoqa@gmail.com>
2018-07-17 22:26:00 +0300

    Add docstrings to Movie class in media.py

--- media.py
+++ media.py
@@ -1,5 +1,18 @@
 class Movie():
+    """A glorious form of entertainment.
+
+    Attributes:
+        title (str):  The title of the movie
+        poster_image_url (str):  The URL of the poster image of the movie
+        trailer_youtube_url:  The youtube URL of the movie
+    """
+
     def __init__(self, movie_title, poster_image, trailer_youtube):
+        """Args:
+            movie_title (str):  The title of the movie
+            poster_image (str):  The URL of the poster image of the movie
+            trailer_youtube (str):  The youtube URL of the movie
+         """
         self.title = movie_title
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
